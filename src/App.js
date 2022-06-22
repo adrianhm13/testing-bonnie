bb0eb2803ffc04ade4064632654343affadfbc26
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-06-22 18:45:18 +0200

    Created button component with its tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("red");
+
+  const onClick = () => {
+    setColor(color === "red" ? "blue" : "red");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        style={{ backgroundColor: color, color: "white" }}
+        onClick={onClick}
+      >
+        {color === "red" ? "Change to blue" : "Change to red"}
+      </button>
     </div>
   );
 }
